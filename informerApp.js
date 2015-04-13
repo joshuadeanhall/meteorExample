b4071b91cf16e9df06280ab82219ab8391c0c5ad
Ubuntu <azureuser@informerDemo.informerDemo.c6.internal.cloudapp.net>
2015-04-13 00:30:01 +0000

    Some changes

--- informerApp.js
+++ informerApp.js
@@ -1,17 +1,44 @@
 // simple-todos.js
-Tasks = new Mongo.Collection("tasks");
 Images = new Mongo.Collection("images");
 if (Meteor.isClient) {
   // This code only runs on the client
-  Template.body.helpers({
-    tasks: function () {
-      return Tasks.find({});
-    },
-    images: function() {
-	return Images.find({});
-  }
-  });
+
+
+Template.imageList.rendered = function() {
+	var self = this;
+	$(".carousel").carousel("pause").removeData();
+	$(".carousel").carousel();
+	console.log('renderingImageList');
+
+	Meteor.subscribe("imageChanged", function() {
+		console.log("real change event");
+	});
+	self.autorun(function() {
+		self.subscribe("imageChanged", function() {
+			console.log('imageChanged event');
+			Tracker.autorun(function() {
+				var query = Images.find({});
+				updateCarousel();
+			});
+		});
+	});
 }
+
+Template.imageList.helpers({
+    images: function() {
+        return Images.find({});
+  },
+}
+);
+
+function updateCarousel() {
+	//alert('updateit');
+	console.log('updating carousel');
+
+}
+
+}
+
 
 
 
@@ -19,4 +46,9 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
+
+Meteor.publish("imageChanged", function() {
+	return Images.find({});
+});
+
 }
