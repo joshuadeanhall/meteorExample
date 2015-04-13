c51c9a1fc54c75f04086c14c3bc836ad2fceb197
Ubuntu <azureuser@informerDemo.informerDemo.c6.internal.cloudapp.net>
2015-04-13 12:50:05 +0000

    clean up code and got it to refresh the carousel when the data changes

--- informerApp.js
+++ informerApp.js
@@ -8,36 +8,17 @@ Template.imageList.rendered = function() {
 	var self = this;
 
 	$(".carousel").carousel();
-	Meteor.subscribe("imageChanged", function() {
-		console.log("real change event");
-	});
 
 	Meteor.autosubscribe(function() {
-
-
-	var cursor = Images.find({});
-
-	var handle = cursor.observe({
-		added: function(item) {
-			alert('added');	
-			$(".carousel").carousel("pause").removeData();
-			$(".carousel").carousel();
-		},
-		changed: function(item) {
-			alert('changednew');
-		}
-	});
-
-	});
-
-	self.autorun(function() {
-		self.subscribe("imageChanged", function() {
-			console.log('imageChanged event');
-			Tracker.autorun(function() {
-				var query = Images.find({});
-				updateFunction();
-			});
+		var cursor = Images.find({});
+		var handle = cursor.observe({
+			added: function(item) {
+				console.log('added')
+				$(".carousel").carousel("pause").removeData();
+				$(".carousel").carousel();
+			}
 		});
+
 	});
 }
 
@@ -48,11 +29,6 @@ Template.imageList.helpers({
 }
 );
 
-function updateFunction() {
-	console.log('updating');
-
-}
-
 }
 
 
@@ -60,9 +36,4 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
-
-Meteor.publish("imageChanged", function() {
-	return Images.find({});
-});
-
 }
