48d14188456a0dc2c6afc4e02c86d300f6c180c0
Ubuntu <azureuser@informerDemo.informerDemo.c6.internal.cloudapp.net>
2015-04-13 01:00:56 +0000

    small fixes"

--- informerApp.js
+++ informerApp.js
@@ -6,19 +6,36 @@ if (Meteor.isClient) {
 
 Template.imageList.rendered = function() {
 	var self = this;
-	$(".carousel").carousel("pause").removeData();
-	$(".carousel").carousel();
-	console.log('renderingImageList');
 
+	$(".carousel").carousel();
 	Meteor.subscribe("imageChanged", function() {
 		console.log("real change event");
 	});
+
+	Meteor.autosubscribe(function() {
+
+
+	var cursor = Images.find({});
+
+	var handle = cursor.observe({
+		added: function(item) {
+			alert('added');	
+			$(".carousel").carousel("pause").removeData();
+			$(".carousel").carousel();
+		},
+		changed: function(item) {
+			alert('changednew');
+		}
+	});
+
+	});
+
 	self.autorun(function() {
 		self.subscribe("imageChanged", function() {
 			console.log('imageChanged event');
 			Tracker.autorun(function() {
 				var query = Images.find({});
-				updateCarousel();
+				updateFunction();
 			});
 		});
 	});
@@ -31,15 +48,12 @@ Template.imageList.helpers({
 }
 );
 
-function updateCarousel() {
-	//alert('updateit');
-	console.log('updating carousel');
+function updateFunction() {
+	console.log('updating');
 
 }
 
 }
-
-
 
 
 if (Meteor.isServer) {
